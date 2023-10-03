1e9b56c70d4e6d866df9f732083f8515c0dbe914
Vardaan Narang <vardaannarang2002@gmail.com>
2023-10-03 23:14:09 +0530

    Update DateTest.java
    - Removed extra spaces

--- java/Java OOP Fundamentals Assignment/DateTest.java
+++ java/Java OOP Fundamentals Assignment/DateTest.java
@@ -4,17 +4,10 @@ public class DateTest {
         Date date1  = new Date(8,28,2000);
         System.out.println("The Initial Date ");
         date1.displayDate();
-
-
         date1.setMonth(2);
         date1.setDay(12);
         date1.setYear(2003);
-
         System.out.println("\nThe Updated Date ");
         date1.displayDate();
-
-
-
-
     }
 }
